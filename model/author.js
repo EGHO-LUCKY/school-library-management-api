dae423010f88ba476ea38370c8439712fe8257fa
EGHO-LUCKY <egholucky8@gmail.com>
2026-03-27 00:52:19 +0100

    Change model to models

--- model/author.js
+++
@@ -1,9 +0,0 @@
-const { Schema, model } = require('mongoose');
-
-const authorSchema = new Schema ({
-    name: { type: String, required: true },
-    bio: { type: String }, 
-    createdAt: { type: Date, default: Date.now }
-});
-
-module.exports = model('Author', authorSchema);
--- model/libraryAttendant.js
+++
@@ -1,8 +0,0 @@
-const { Schema, model, Types } = require('mongoose');
-
-const libraryAttendantSchema = new Schema({
-    name: { type: string, required: true },
-    createdAt: { type: Date, default: Date.now }
-});
-
-module.exports = model('Attendant', libraryAttendantSchema);
