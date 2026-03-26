8fd43ba63ce539e07331d92e400a5560adc37d52
EGHO-LUCKY <egholucky8@gmail.com>
2026-03-27 00:35:51 +0100

    Add unique and required property to isbn field

---
+++ models/book.js
@@ -0,0 +1,14 @@
+const { Schema, model, Types } = require('mongoose');
+
+const bookSchema = new Schema({
+    title: { type: String, required: true, trim: true },
+    isbn: { type: String, required: true, unique: true },
+    authors: [{ type: Schema.Types.ObjectId, ref: "Author" }],
+    status: { type: String, enum: ["IN", "OUT"], default: "IN" },
+    borrowedBy: { type: Schema.Types.ObjectId, ref: "Student", default: null },
+    issuedBy: { type: Schema.Types.ObjectId, ref: "Attendant", default: null },
+    returnDate: { type: Date, default: null },
+    createdAt: { type: Date, default: Date.now }
+});
+
+module.exports = model('Book', bookSchema);
