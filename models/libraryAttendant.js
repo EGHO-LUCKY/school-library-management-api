c1d19888e7fd5b3319c8dbf8c253b6fbdad76b23
EGHO-LUCKY <egholucky8@gmail.com>
2026-04-03 22:48:30 +0100

    Delete libraryAttendant model

--- models/libraryAttendant.js
+++
@@ -1,8 +0,0 @@
-const { Schema, model, Types, trusted } = require('mongoose');
-
-const libraryAttendantSchema = new Schema({
-    name: { type: String, required: true, trim: true },
-    createdAt: { type: Date, default: Date.now }
-});
-
-module.exports = model('Attendant', libraryAttendantSchema);
