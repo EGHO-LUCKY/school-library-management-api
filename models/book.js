ef623621c1e83e348052b76bc2cb3df8d470cc3c
EGHO-LUCKY <egholucky8@gmail.com>
2026-04-03 22:47:57 +0100

    Refactor bookSchema

--- models/book.js
+++ models/book.js
@@ -1,12 +1,12 @@
-const { Schema, model, Types } = require('mongoose');
+const { Schema, model } = require('mongoose');
 
 const bookSchema = new Schema({
     title: { type: String, required: true, trim: true },
     isbn: { type: String, required: true, unique: true },
     authors: [{ type: Schema.Types.ObjectId, ref: "Author" }],
     status: { type: String, enum: ["IN", "OUT"], default: "IN" },
-    borrowedBy: { type: Schema.Types.ObjectId, ref: "Student", default: null },
-    issuedBy: { type: Schema.Types.ObjectId, ref: "Attendant", default: null },
+    borrowedBy: { type: Schema.Types.ObjectId, ref: "User", default: null },
+    issuedBy: { type: Schema.Types.ObjectId, ref: "User", default: null },
     returnDate: { type: Date, default: null },
     createdAt: { type: Date, default: Date.now }
 });
