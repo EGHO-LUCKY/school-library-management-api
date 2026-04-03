bf2c6f5c7bad95b27f923d4b832554b179e621f9
EGHO-LUCKY <egholucky8@gmail.com>
2026-04-03 22:57:29 +0100

    Add authorization and role middleware

--- routes/bookRoute.js
+++ routes/bookRoute.js
@@ -1,13 +1,15 @@
 const router = require('express').Router();
 const bookController = require('../controllers/bookController');
 const validate = require('../middleware/validate');
+const { authenticate, authorizeRole } = require('../middleware/auth');
 
-router.post('/', bookController.createBook);
-router.get('/', bookController.getBooks);
-router.get('/:id', validate.validateId, bookController.getBookById);
-router.put('/:id', validate.validateId, bookController.updateBookById);
-router.delete('/:id', validate.validateId, bookController.deleteBookById);
-router.post('/:id/borrow', validate.validateId, validate.validateDate, bookController.borrowBook);
-router.post('/:id/return', validate.validateId, bookController.returnBook);
+
+router.post('/books', authenticate, authorizeRole('admin'), bookController.createBook);
+router.get('/books', authenticate, bookController.getBooks);
+router.get('/books/:id',authenticate, validate.validateId, bookController.getBookById);
+router.put('/books/:id', authenticate, authorizeRole('admin'), validate.validateId, bookController.updateBookById);
+router.delete('/books/:id', authenticate, authorizeRole('admin'), validate.validateId, bookController.deleteBookById);
+router.post('/books/:id/borrow', authenticate, authorizeRole('attendant'), validate.validateId, validate.validateDate, bookController.borrowBook);
+router.post('/books/:id/return', authenticate, authorizeRole('attendant'), validate.validateId, bookController.returnBook);
 
 module.exports = router;
