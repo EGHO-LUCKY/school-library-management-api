a4664730dcb10ad2ee620d31d3ef4624775596e2
EGHO-LUCKY <egholucky8@gmail.com>
2026-04-03 22:55:30 +0100

    Delete libraryAttendantRoute

--- routes/libraryAttendantRoute.js
+++
@@ -1,7 +0,0 @@
-const router = require('express').Router();
-const attendantController = require('../controllers/libraryAttendantController');
-
-router.post('/', attendantController.createAttendant);
-router.get('/', attendantController.getAttendants);
-
-module.exports = router;
