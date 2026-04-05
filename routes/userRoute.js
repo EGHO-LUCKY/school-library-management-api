36d5a1b4c849f15a7b78ed5733145c87b2d6c4ed
EGHO-LUCKY <egholucky8@gmail.com>
2026-04-05 20:56:32 +0100

    Implement attendantRoute and studentRoute in userRoute

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -1,8 +1,22 @@
 const router = require('express').Router();
 const userController = require('../controllers/userController');
+const validate = require('../middleware/validate');
+const { authenticate, authorizeRole } = require('../middleware/auth');
 
 
-router.post('/register', userController.createUser);
-router.get('/login', userController.loginUser);
+// registration and login route
+router.post('/auth/register', userController.createUser);
+router.post('/auth/login', userController.loginUser);
+
+
+// students route
+router.get('/students', authenticate, authorizeRole('admin', 'attendant'), userController.getUsers("student"));
+router.get('/students/:id', authenticate, authorizeRole('admin', 'attendant'), validate.validateId, userController.getUserById("student"));
+
+
+// attendants route
+router.get('/attendants', authenticate, authorizeRole('admin'), userController.getUsers("attendant"));
+router.get('/attendants/:id', authenticate, authorizeRole('admin'), validate.validateId, userController.getUserById("attendant"));
+
 
 module.exports = router;
